cb8d2180b6af1151a5e6e994fb1301667a3ab55b
Paul Arntz <paul@paularntz.com>
2022-07-10 16:40:18 -0500

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,85 +1,85 @@
+require("dotenv").config({
+  path: `.env`,
+})
 
-  require("dotenv").config({
-    path: `.env`,
-  });
-
-export const siteMetadata = {
-  title: `AltRoots Trademark Watch`,
-  author: {
-    name: `EnCirca Inc.`,
-    summary: `SOC 2 AUDITED`,
-  },
-  description: `AltRoots Blog`,
-  siteUrl: `https://www.alt-roots.com/`,
-  social: {
-    twitter: `encirca`,
-    instagram: `encirca`,
-    github: `encirca`
-  },
-  numPostsPerPage: 10 // For demonstration of pagination, you'll likely want to increase this
-}
-export const plugins = [
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      path: `${__dirname}/content/blog`,
-      name: `blog`,
+module.exports = {
+  siteMetadata: {
+    title: `AltRoots Trademark Watch`,
+    author: {
+      name: `EnCirca Inc.`,
+      summary: `SOC 2 AUDITED`,
     },
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      path: `${__dirname}/content/pages`,
-      name: `pages`,
+    description: `AltRoots Blog`,
+    siteUrl: `https://www.alt-roots.com/`,
+    social: {
+      twitter: `encirca`,
+      instagram: `encirca`,
+      github: `encirca`
     },
+    numPostsPerPage: 10 // For demonstration of pagination, you'll likely want to increase this
   },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      path: `${__dirname}/content/assets`,
-      name: `assets`,
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
     },
-  },
-  {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      plugins: [
-        {
-          resolve: `gatsby-remark-images`,
-          options: {
-            maxWidth: 590,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/pages`,
+        name: `pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
           },
-        },
-        {
-          resolve: `gatsby-remark-responsive-iframe`,
-          options: {
-            wrapperStyle: `margin-bottom: 1.0725rem`,
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
           },
-        },
-        `gatsby-remark-prismjs`,
-        `gatsby-remark-copy-linked-files`,
-        `gatsby-remark-smartypants`,
-      ],
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
     },
-  },
-  `gatsby-transformer-sharp`,
-  `gatsby-plugin-sharp`,
-  {
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
-      trackingId: `310314792`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `310314792`,
+      },
     },
-  },
-  {
-    resolve: `gatsby-plugin-sitemap`,
-    options: {
-      output: `/sitemap.xml`
+    {
+      resolve:`gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`
+      },
     },
-  },
-  {
+    {
     resolve: `gatsby-plugin-feed`,
-    options: {
-      query: `
+      options: {
+        query: `
           {
             site {
               siteMetadata {
@@ -91,20 +91,20 @@ export const plugins = [
             }
           }
         `,
-      feeds: [
-        {
-          serialize: ({ query: { site, allMarkdownRemark } }) => {
-            return allMarkdownRemark.nodes.map(node => {
-              return Object.assign({}, node.frontmatter, {
-                description: node.excerpt,
-                date: node.frontmatter.date,
-                url: site.siteMetadata.siteUrl + node.fields.slug,
-                guid: site.siteMetadata.siteUrl + node.fields.slug,
-                custom_elements: [{ "content:encoded": node.html }],
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
+                })
               })
-            })
-          },
-          query: `
+            },
+            query: `
               {
                 allMarkdownRemark(
                   sort: { order: DESC, fields: [frontmatter___date] },
@@ -123,39 +123,40 @@ export const plugins = [
                 }
               }
             `,
-          output: "/rss.xml",
-          title: "Your Site's RSS Feed",
-          // optional configuration to insert feed reference in pages:
-          // if `string` is used, it will be used to create RegExp and then test if pathname of
-          // current page satisfied this regular expression;
-          // if not provided or `undefined`, all pages will have feed reference inserted
-          match: "^/blog/",
-          // optional configuration to specify external rss feed, such as feedburner
-          link: "https://feeds.feedburner.com/gatsby/blog",
-        },
-      ],
+            output: "/rss.xml",
+            title: "Your Site's RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/blog/",
+            // optional configuration to specify external rss feed, such as feedburner
+            link: "https://feeds.feedburner.com/gatsby/blog",
+          },
+        ],
+      },
     },
-  },
-  `gatsby-plugin-react-helmet`,
-  {
-    resolve: `gatsby-plugin-google-fonts`,
-    options: {
-      fonts: [
-        `Nunito Sans:700,300`,
-        `Crimson Text:700,400,400i`
-      ],
-      display: 'swap'
-    }
-  },
-  {
-    resolve: `gatsby-source-stripe`,
-    options: {
-      objects: ["Price"],
-      secretKey: process.env.STRIPE_SECRET_KEY,
-      downloadFiles: false,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Nunito Sans:700,300`,
+          `Crimson Text:700,400,400i`
+        ],
+        display: 'swap'
+      }
     },
-  },
-  // this (optional) plugin enables Progressive Web App + Offline functionality
-  // To learn more, visit: https://gatsby.dev/offline
-  // `gatsby-plugin-offline`,
-]
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ["Price"],
+        secretKey: process.env.STRIPE_SECRET_KEY,
+        downloadFiles: false,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
