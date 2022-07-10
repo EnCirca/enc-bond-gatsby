1d821e8ffc2662c189c668656610c7f4c02788d1
Paul Arntz <paul@paularntz.com>
2022-07-10 14:55:37 -0500

    add stripe and more form updates

--- src/components/watch-form.js
+++ src/components/watch-form.js
@@ -1,8 +1,14 @@
 import React from "react"
 import styles from "./watch-form.module.css"
 import ReCAPTCHA from "react-google-recaptcha"
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
 const WatchForm = () => {
+
+    const processForm = () => {
+        console.log('processing');
+    }
 
     return (
         <div id="encirca-watch-form" className={styles.watchform}>
@@ -13,29 +19,52 @@ const WatchForm = () => {
                 data-netlify="true" 
                 data-netlify-recaptcha="true"
                 action="/thank-you"
+                onSubmit={processForm}
             >
-            <input type="hidden" name="form-name" value="enc-watch-form" />
-            <div>
-                <label>Your Name:<br /><input type="text" name="name" required="true" /></label>
-            </div>
-            <div>
-                <label>Company Name:<br /><input type="text" name="company-name" /></label>
-            </div>
-            <div>
-                <label>Email Address:<br />
-                <small>A corporate email address must be used, not Gmail, Hotmail, Yahoo etc...</small><br />
-                <input type="email" name="email" required="true" /></label>
-            </div>
-            <div>
-                <label>Watch Terms:<br />
-                <small>Please enter the words or brand names you want us to watch. For example: acme, kleenex, ibm etc...</small><br />
-                <textarea name="watch-terms" required="true" ></textarea></label>
-            </div>
-            <div>
-                <ReCAPTCHA sitekey="6LcBENYgAAAAABttHtD7kvkByQKqlV7KSZy9zEqB" />
-                <button type="submit">Send</button>
-            </div>
-            <input type="hidden" name="form-name" value="watch-form" />
+                <input type="hidden" name="form-name" value="enc-watch-form" />
+                <div>
+                    <label>
+                        Full Name:<br />
+                        <input type="text" name="name" required="true" />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Company Website:<br />
+                        <input type="text" name="company-name" />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Work Email:<br />
+                        <small>Work Email domain must match company website domain. Free or disposable email addresses are not allowed.</small><br />
+                        <input type="email" name="email" required="true" />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Watch Terms (optional):<br />
+                        <small>Please enter up to 10 trademarks for a free status report.</small><br />
+                        <textarea name="watch-terms" required="true" ></textarea>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input type="checkbox" name="webinar-schedule-request" />
+                        Send me your webinar schedule
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input type="checkbox" name="terms-read" />
+                        I have read the <a rel="noreferrer" target="_blank" href="https://www.encirca.com/terms">terms of service</a>.
+                    </label>
+                </div>
+                <div>
+                    <ReCAPTCHA sitekey="6LcBENYgAAAAABttHtD7kvkByQKqlV7KSZy9zEqB" />
+                    <button type="submit">Send</button>
+                </div>
+                <input type="hidden" name="form-name" value="watch-form" />
             </form>
         </div>
     )
