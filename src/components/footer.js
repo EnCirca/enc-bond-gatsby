efd41b80c4871191e89105ae841f5e4a59e6c924
Paul Arntz <paul@paularntz.com>
2022-07-14 10:31:01 -0500

    updates to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -35,7 +35,7 @@ const Footer = () => {
             <nav>
                 <ul className={styles.menu}>
                     <li className={styles.item}><a href="/">Home</a></li>
-                    <li className={styles.item}><a target="_blank" href="https://www.encirca.com">EnCirca Inc.</a></li>
+                    <li className={styles.item}><a rel="noreferrer" target="_blank" href="https://www.encirca.com">EnCirca Inc.</a></li>
                     <li className={styles.item}><a href="/faq">FAQ</a></li>
                     <li className={styles.item}><a href="/glossary">Glossary</a></li>
                     <li className={styles.item}><a href="/terms">Terms</a></li>
