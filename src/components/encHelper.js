0f4f934debca3f4a3136999098b3ca441b2635f8
Paul Arntz <paul@paularntz.com>
2022-07-14 11:42:42 -0500

    adding force company email back to request more info form

--- src/components/encHelper.js
+++ src/components/encHelper.js
@@ -3,17 +3,17 @@ import React, { useEffect } from "react";
 const EncHelper = () => {
   useEffect(() => {
     
-    // const emailNotAllowed = ["gmail.com", "msn.com", "aol.com", "yahoo.com", "mac.com", "hotmail.com", "icloud.com"]
-    // const rmiInput = document.getElementById('more-info-company')
-    // if (!rmiInput) {
-    //   rmiInput.addEventListener('keyup', (e) => {
-    //     emailNotAllowed.forEach(addy => {
-    //       if (rmiInput.value.includes(addy)) {
-    //         rmiInput.value = rmiInput.value.replace(addy, '')
-    //       }
-    //     })
-    //   })
-    // }
+    const notAllowed = ["gmail.com", "msn.com", "aol.com", "yahoo.com", "mac.com", "hotmail.com", "icloud.com"]
+    const rmiInput = document.getElementById('more-info-company')
+    if (rmiInput && rmiInput !== 'undefined') {
+      rmiInput.addEventListener('keyup', (e) => {
+        notAllowed.forEach(addy => {
+          if (rmiInput.value.includes(addy)) {
+            rmiInput.value = rmiInput.value.replace(addy, '')
+          }
+        })
+      })
+    }
 
   }, []);
   return (
